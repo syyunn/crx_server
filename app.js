8cf0226256e894edd2f7627da5f5e205db9e0e96
Zachary <syyun@snu.ac.kr>
2019-06-14 09:41:38 +0900

    Discard res arg

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/example', (req, ) => {
+    console.log(`${req.body.hlgt}`);
+});
 
 const port = 8080;
 
@@ -9,7 +14,4 @@ app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
 
-app.post('/example', (req, res) => {
-    console.log(`${req.body.name}`);
-});
 
