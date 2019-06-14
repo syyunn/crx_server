38b9451f56c54d2cde7d73b81b09246619fb7989
Zachary <syyun@snu.ac.kr>
2019-06-14 18:32:22 +0900

    Add python runs in express js

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const python = require('python-shell');
+const python_path = '/Users/zachary/anaconda3/bin/python';
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/example', (req, ) => {
-    console.log(`${req.body.hlgt}`);
+app.post('/client_txt', (req, ) => {
+
+    const text = `${req.body.hlgt}`;
+
+    console.log(text);
+
+    /* python runs in express js */
+    const options = {
+        mode: 'text',
+        pythonPath: python_path,
+        pythonOptions: ['-u'],
+        scriptPath: 'test.py',
+        args: ['--text', text]
+    };
+
+    python.PythonShell.run('.', options,
+        function (err, results) {
+        if (err)
+            throw err;
+        console.log('results: %j', results); // python return val stored in results
+    });
+
+    /******************************/
+
 });
 
 const port = 8080;
@@ -13,5 +38,3 @@ const port = 8080;
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
-
-
